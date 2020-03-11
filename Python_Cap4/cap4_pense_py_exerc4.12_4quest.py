827e4428a4e45c96c0c094ce9b7a0131a9fd6071
HalysonItallo <halyssonpimentell@gmail.com>
2020-03-11 01:36:34 -0300

    Consegui

--- Python_Cap4/cap4_pense_py_exerc4.12_4quest.py
+++ Python_Cap4/cap4_pense_py_exerc4.12_4quest.py
@@ -1,18 +1,58 @@
 import turtle
 bob = turtle.Turtle()
+alice = turtle.Turtle()
+walter = turtle.Turtle()
 
+def position_turtle():
+    alice.pu()
+    alice.setposition(-300,0)
+    alice.pd()
+    
+    walter.pu()
+    walter.setposition(300,0)
+    walter.pd()
 
 def polygon(t,length,n):
     angle = 360 / n
     for _ in range(n):
         t.fd(length)
         t.lt(angle)
-    bob.pu()
-    bob.home()
        
-def radial():
 
-    angle = int(360 / 6)
+def radial_5():
+    alice.pd()
+    alice.lt(55)
+    alice.fd(85)
+    
+    alice.lt(252)
+    alice.fd(85)
+    
+    alice.lt(180)
+    alice.fd(85)
+       
+    alice.lt(35)
+    alice.fd(84)
+       
+       
+    alice.lt(180)
+    alice.pu()
+    alice.fd(85)
+    alice.pd()
+    
+    alice.lt(35)
+    alice.fd(84)
+    
+       
+    alice.lt(180)
+    alice.pu()
+    alice.fd(85)
+    alice.pd()
+    
+    alice.lt(253)
+    alice.fd(84)
+     
+       
+def radial_6():
     bob.pd()
     bob.lt(60)
     bob.fd(100)
@@ -37,17 +77,74 @@ def radial():
     bob.lt(241)
     bob.fd(100)
     
+def radial_7():
+    walter.pd()
+    walter.lt(65)
+    walter.fd(115)
+    
+    walter.lt(232)
+    walter.fd(115)
+    
+    walter.pu()
+    walter.lt(180)
+    walter.fd(115)
+    walter.pd()
+    
+    walter.lt(76)
+    walter.fd(115)
+    
+    walter.pu()
+    walter.lt(180)
+    walter.fd(115)
+    walter.pd()
     
     
+    walter.lt(129)
+    walter.fd(115)
+    
+    walter.pu()
+    walter.lt(180)
+    walter.fd(115)
+    walter.pd()
+    
+    walter.lt(128)
+    walter.fd(115)
+    
+    walter.pu()
+    walter.lt(180)
+    walter.fd(115)
+    walter.pd()
+    
+    walter.lt(128)
+    walter.fd(115)
+    
+    walter.pu()
+    walter.lt(180)
+    walter.fd(115)
+    walter.pd()
+    
+    walter.lt(128)
+    walter.fd(115)
+    
+    walter.pu()
+    walter.lt(180)
+    walter.fd(115)
+    walter.pd()
     
     
-    
-   
-
     
 def main():
+    position_turtle()
+    
+    polygon(alice,100,5)
+    radial_5()
+    
     polygon(bob,100,6)
-    radial()
+    radial_6()
+
+    polygon(walter,100,7)
+    radial_7()
+    
     turtle.mainloop()    
 
 main()
