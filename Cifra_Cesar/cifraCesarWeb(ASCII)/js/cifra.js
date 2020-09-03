083ac2ad61fb35982ea72c607cbd42a40075c1a7
HalysonItallo <halyssonpimentell@gmail.com>
2020-09-03 09:35:14 -0300

    CifraWeb usando a tabela ASCII

---
+++ Cifra_Cesar/cifraCesarWeb(ASCII)/js/cifra.js
@@ -0,0 +1,53 @@
+const text = document.getElementById('text-cifra');
+const number = document.getElementById('number-cifra');
+const button = document.getElementById('button');
+const content = document.getElementById('show-result');
+
+function buildText(text, indice, displacementNumber) {
+  const posLetterDisplace = text.charCodeAt(indice) + displacementNumber;
+  const posLetter = text.charCodeAt(indice);
+  let resultAphabet = '';
+  if (65 <=  posLetter && posLetter <= 90) { 
+    if (posLetterDisplace < 65 && displacementNumber < 0) {
+      resultAphabet = String.fromCharCode(90 + (posLetterDisplace - 64));
+    } else if (90 < posLetterDisplace && displacementNumber > 0 ){
+      resultAphabet = String.fromCharCode((posLetterDisplace - 90) + 64);
+    } else { 
+      resultAphabet = String.fromCharCode(posLetterDisplace);
+    }
+  }
+  if (97 <=  posLetter && posLetter <= 122) { 
+    if (posLetterDisplace < 97 && displacementNumber < 0) {
+      resultAphabet = String.fromCharCode(122 + (posLetterDisplace - 96));
+    } else if (122 < posLetterDisplace && displacementNumber > 0 ){
+      resultAphabet = String.fromCharCode((posLetterDisplace - 122) + 96);
+    } else { 
+      resultAphabet = String.fromCharCode(posLetterDisplace);
+    }
+  }
+  if (posLetter == 32) { 
+    resultAphabet = ' '; 
+  }
+  
+  return resultAphabet;
+}
+
+function rotateWord(e) {
+  e.preventDefault();
+  
+  const displacementNumber = Number(number.value);
+  content.innerHTML = '';
+  
+  let unencryptedText = '';
+  
+  for(let i = 0; i < text.value.length; i++) {
+    unencryptedText += buildText(text.value, i, displacementNumber);
+  } 
+  
+  const showText = document.createElement('h3');
+  showText.textContent = unencryptedText;
+  content.appendChild(showText);
+}
+
+button.addEventListener('click', rotateWord);
+
--- Cifra_Cesar/cifraCesarWeb/js/cifra.js
+++ Cifra_Cesar/cifraCesarWeb/js/cifra.js
@@ -1,42 +1,42 @@
-const text = document.getElementById('text-cifra')
-const number = document.getElementById('number-cifra')
-const button = document.getElementById('button')
-const content = document.getElementById('show-result')
+const text = document.getElementById('text-cifra');
+const number = document.getElementById('number-cifra');
+const button = document.getElementById('button');
+const content = document.getElementById('show-result');
 
 function buildArray(index, alphabet, displacementNumber) {
-  let resultAphabet = ''
+  let resultAphabet = '';
   if (alphabet[index+displacementNumber] == undefined && displacementNumber >= 0) { 
-    resultAphabet += alphabet[(displacementNumber - (alphabet.length - index))]
+    resultAphabet += alphabet[(displacementNumber - (alphabet.length - index))];
   } else if (alphabet[index+displacementNumber] == undefined && displacementNumber < 0) {
-    resultAphabet += alphabet[alphabet.length + (displacementNumber + index)] 
+    resultAphabet += alphabet[alphabet.length + (displacementNumber + index)]; 
   }
   else {
-    resultAphabet += alphabet[index+displacementNumber]
+    resultAphabet += alphabet[index+displacementNumber];
   }
-  return resultAphabet
+  return resultAphabet;
 }
 
 function rotateWord(e) {
-  e.preventDefault()
+  e.preventDefault();
   
-  const displacementNumber = Number(number.value)
-  content.innerHTML = ''
+  const displacementNumber = Number(number.value);
+  content.innerHTML = '';
   
   const lowercaseAlphabet = ["a","b","c","d","e","f","g","h","i","j",
-  "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const uppercaseAlphabet = ["A","B","C","D","E","F","G","H","I","J",
-  "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-  let unencryptedText = ''
+  let unencryptedText = '';
   
   for(let i = 0; i < text.value.length; i++) {
     for(let j = 0; j < lowercaseAlphabet.length; j++) {
       if(text.value[i] == lowercaseAlphabet[j]) {
-        unencryptedText += buildArray(j, lowercaseAlphabet, displacementNumber)
+        unencryptedText += buildArray(j, lowercaseAlphabet, displacementNumber);
       } else if(text.value[i] == ' ' && j == lowercaseAlphabet.length - 1) {
-        unencryptedText += ' '
+        unencryptedText += ' ';
       }  else if(text.value[i] == uppercaseAlphabet[j]) {
-        unencryptedText += buildArray(j, uppercaseAlphabet, displacementNumber)
+        unencryptedText += buildArray(j, uppercaseAlphabet, displacementNumber);
       }
       
     }
